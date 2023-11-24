ccda24c6366e2500e4622ccd3172e179be0c9a2d
Game Black <71898409+GameBlackRC@users.noreply.github.com>
2023-11-24 13:55:46 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -30,17 +30,21 @@ function randomPokeAccueil() {
 
     let sprite = 'regular';
 
+    let code = "";
 
     if(random === 1) {
 
         sprite = 'shiny';
         console.log('shiny')
 
+        code += "<img onclick=clickRandomPoke(" + poke['pokedexId'] + ") src='" + poke['sprites']["shiny"] + "'></img><div class='info_random'><p>#" + poke['pokedexId'] + " " + poke['name']['fr'] + "</p><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='#FDE966' stroke-width='3' stroke-linecap='round' stroke-linejoin='round' class='feather feather-star'><polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2'/></svg><div class='types'>";
+
     }
+    else {
 
-    let code = "";
+        code += "<img onclick=clickRandomPoke(" + poke['pokedexId'] + ") src='" + poke['sprites']["regular"] + "'></img><div class='info_random'><p>#" + poke['pokedexId'] + " " + poke['name']['fr'] + "</p><div class='types'>";
 
-    code += "<img onclick=clickRandomPoke(" + poke['pokedexId'] + ") src='" + poke['sprites'][sprite] + "'></img><div class='info_random'><p>#" + poke['pokedexId'] + " " + poke['name']['fr'] + "</p><div class='types'>";
+    }
 
     types.forEach(function (type) {
 
@@ -69,12 +73,13 @@ function fichePokeDetail(id, balise) {
     $(balise).html();
 
     let poke = liste_poke.filter((poke_) => poke_['pokedexId'] == id)[0];
+    console.log(poke);
 
     if(poke['types'] == undefined) {
 
         let contenu = "<div class='fiche_poke_detail'><h2 style='text-align: center;'>#" + poke["pokedexId"] + " " + poke["name"]["fr"] + "</h2><div class='infos'><img class='img_poke_detail' src='" + poke["sprites"]["regular"] + "' width='25%'><div class='infos_texte'><h3>Types :</h3><br><div class='types'></div><h3>Génération : " + poke["generation"] + "</h3><div class='evolution'>";
         console.log(poke["evolution"]);
-        if(poke["evolution"]["pre"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["pre"] != null) {
 
             contenu += "<h3>Pré-évolution</h3><br>";
 
@@ -85,7 +90,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["next"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["next"] != null) {
 
             contenu += "<h3>Évolution</h3><br>";
 
@@ -96,7 +101,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["mega"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["mega"] != null) {
 
             contenu += "<h3>Méga-évolution</h3><br>";
 
@@ -108,6 +113,8 @@ function fichePokeDetail(id, balise) {
 
         }
         contenu += "</div></div></div></div>";
+
+        $(balise).html(contenu);
 
     }
     else if(poke['types'][1] == undefined) {
@@ -116,7 +123,7 @@ function fichePokeDetail(id, balise) {
 
         let contenu = "<div class='fiche_poke_detail'><h2 style='text-align: center;'>#" + poke["pokedexId"] + " " + poke["name"]["fr"] + "</h2><div class='infos'><img class='img_poke_detail' src='" + poke["sprites"]["regular"] + "' width='25%'><div class='infos_texte'><h3>Types :</h3><br><div class='types'><img src='" + poke["types"][0]["image"] + "' width='3%'></div><h3>Génération : " + poke["generation"] + "</h3><div class='evolution'>";
         console.log(poke["evolution"]);
-        if(poke["evolution"]["pre"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["pre"] != null) {
 
             contenu += "<h3>Pré-évolution</h3><br>";
 
@@ -127,7 +134,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["next"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["next"] != null) {
 
             contenu += "<h3>Évolution</h3><br>";
 
@@ -138,7 +145,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["mega"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["mega"] != null) {
 
             contenu += "<h3>Méga-évolution</h3><br>";
 
@@ -151,12 +158,14 @@ function fichePokeDetail(id, balise) {
         }
         contenu += "</div></div></div></div>";
 
+        $(balise).html(contenu);
+
     }
     else {
 
         let contenu = "<div class='fiche_poke_detail'><h2 style='text-align: center;'>#" + poke["pokedexId"] + " " + poke["name"]["fr"] + "</h2><div class='infos'><img class='img_poke_detail' src='" + poke["sprites"]["regular"] + "' width='20%'><div class='infos_texte'><h3>Types :</h3><br><div class='types'><img src='" + poke["types"][0]["image"] + "' width='3%'><img src='" + poke["types"][1]["image"] + "' width='3%'></div><h3>Génération : " + poke["generation"] + "</h3><div class='evolution'>";
         console.log(poke["evolution"]);
-        if(poke["evolution"]["pre"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["pre"] != null) {
 
             contenu += "<h3>Pré-évolution</h3><br>";
 
@@ -167,7 +176,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["next"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["next"] != null) {
 
             contenu += "<h3>Évolution</h3><br>";
 
@@ -178,7 +187,7 @@ function fichePokeDetail(id, balise) {
             })
 
         }
-        if(poke["evolution"]["mega"] != null) {
+        if(poke["evolution"] != null && poke["evolution"]["mega"] != null) {
 
             contenu += "<h3>Méga-évolution</h3><br>";
 
